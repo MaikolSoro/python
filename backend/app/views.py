f074c18bdda45dd88e1ea95af0e30634af0d206a
Maikol Soro <maikolsoro.z1998@gmail.com>
2020-08-21 17:54:20 -0600

    Layout, bloques y herencia de plantillas

--- backend/app/views.py
+++ backend/app/views.py
@@ -8,12 +8,19 @@ def index(request):
 	template = """
 		<h1>Inicio</h1>
 	"""
-	return HttpResponse(template)
+	return render(request, 'index.html')
 def app(request):
 	return HttpResponse("Hola mundo Django!!")
 
-def pagina(request, redirect=0):
-	return redirect('/inicio/')
-	return HttpResponse("""
-		<h1>Página de mi web </h1>
-	""")
+def hola_mundo(request):
+	return render(request, 'hola_mundo.html')
+
+
+def pagina(request, redirigir=0):
+
+	if redirigir == 1:
+		return redirect('contacto', nombre ='Maikol', apeliidos="Soro Zúñiga")
+	
+	return render(request, 'pagina.html')
+	
+
